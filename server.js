b7a4a38e28a5acdec9f531e88e60852744ccb1b2
prince <getforprince@hotmail.com>
2017-07-19 15:46:07 +0530

    first add

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 var express=require('express');
 var app=express();
 var PORT=process.env.PORT||3000;
+var todos=[{
+	id: 1,
+	description: "meet mom for lunch",
+	completed: false
+},{
+	id: 2,
+	description: 'go to market',
+	completed: false
+},{
+	id: 3,
+	description: 'fill the bottel',
+	completed: false
+}];
 
 app.get('/',function(req,res){
 	res.send('todo api root');
-})
+});
+
+app.get('/todos',function(req,res){
+	res.json(todos);
+});
+
+app.get('/todos/:id',function(req,res){
+	var todoId = req.params.id;
+	var matchedTodo;
+	res.send('asking for todo with id of'+ req.params.id);
+
+	todos.forEach(function(todo){
+		if(todoId===todo.id)
+			matchedTodo=todo;
+	});
+	
+	if(matchedTodo){
+		res.json(matchedTodo);
+	}
+	else{
+		res.status(404).send();
+	}
+
+});
+
+
 app.listen(PORT,function(){
-	console.log('express listening on port '+PORT+'!');
+	console.log('express listening on port '+PORT+' !');
 });
---
+++ tesst.js
@@ -0,0 +1,4 @@
+var a=[1,2,3,4,5,6,7,8,9,10];
+a.forEach(function(){
+console.log(a);
+});
