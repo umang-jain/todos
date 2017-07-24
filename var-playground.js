7887900618bc32eabb0e897f924df41be6aa4f42
get4prince <getforprince@hotmail.com>
2017-07-24 15:05:36 +0530

    add filter

---
+++ var-playground.js
@@ -0,0 +1,23 @@
+// var person= {
+// 	name: 'prince',
+// 	age: 19
+// };
+// function updatePerson(obj)
+//  {
+// // 	obj={
+// // 		name : 'prince',
+// // 		age: 72
+// // 	}
+// 	obj.age=2821;
+// }
+// updatePerson(person);
+// console.log(person);
+
+//array
+var arr=[15,37];
+function updateArr(a){
+	a.push(55);
+	debugger;
+}
+updateArr(arr);
+console.log(arr);
