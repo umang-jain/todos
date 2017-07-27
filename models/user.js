fe15d9e63ec25209b6e868a88cf0d3e4eea5016a
get4prince <getforprince@hotmail.com>
2017-07-28 01:46:22 +0530

    convert e-mail to lower case

--- models/user.js
+++ models/user.js
@@ -1,19 +1,28 @@
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true,
-				validate: {
-					isEmail: true
-				}
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				validate: {
-					len: [7, 100]
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, option) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
 				}
 			}
+
+		}
 	});
 }
